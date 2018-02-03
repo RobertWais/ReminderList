b41484cf7a1d44c4274db3cb68ab496e142aad94
RobertWais <wais.robert@uwlax.edu>
2018-02-02 18:58:52 -0600

    Initial Commit

---
+++ Reminders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by Robert Wais on 2/2/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
