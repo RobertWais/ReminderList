cfb014cb44d098731a4628dacb63718b80cd2e72
RobertWais <wais.robert@uwlax.edu>
2018-02-10 23:46:43 -0600

    Added local notifications

--- Reminders/Controllers/CreateReminderVC.swift
+++ Reminders/Controllers/CreateReminderVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class CreateReminderVC: UIViewController {
 
@@ -18,6 +19,17 @@ class CreateReminderVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ///
+        print("Here")
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { (granted, error) in
+            if granted {
+                print("This application has access")
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+        
+        ///
         //extension function that was written
         nextBtn.bindToKeyboard()
         // Do any additional setup after loading the view.
@@ -47,7 +59,16 @@ class CreateReminderVC: UIViewController {
     func saveReminder(completion: (_ finished: Bool)->()){
         guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
         let theDate = datePicker.date.dayWeek()
-        print("The date: \(theDate)")
+        
+        
+        ////
+        let temp = datePicker.calendar.dateComponents([.day,.month,.year,.minute,.second], from: datePicker.date)
+        
+        createNofication(dateComponent: temp, stringNotif: textBox.text, completion: {success in
+            if success {
+                print("Successful")
+            }
+        })
         
         let reminder = Reminder(context: managedContext)
         reminder.nameDescription = textBox.text
@@ -64,6 +85,27 @@ class CreateReminderVC: UIViewController {
         }
         
     }
+    
+    func createNofication(dateComponent: DateComponents, stringNotif: String, completion: @escaping (_ Success: Bool) -> ()) {
+        let notification = UNMutableNotificationContent()
+        notification.title = "Reminder"
+        notification.subtitle = ""
+        notification.body = stringNotif
+        
+        let notifTrigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "datePickerNotif", content: notification, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    }
+    
 }
 
 //Extension for formatting date into,.. ex. Saturday 10:59 PM
